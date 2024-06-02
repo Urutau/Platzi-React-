af4c93e90b45c534b31100133e9aa900e279de21
Urutau <xristinaortega@gmail.com>
2024-06-01 22:29:55 -0300

    Se agregan los svg, se agrega funcionalidad a los íconos

--- src/App.js
+++ src/App.js
@@ -22,7 +22,31 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
-  console.log('Los usuarios buscan ToDos de '+searchValue);
+  const searchedTodos = todos.filter(
+   (todo) =>  {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+   }
+  )
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+  
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
 
   return (
     <>
@@ -37,11 +61,13 @@ function App() {
       />
 
       <TodoList>
-      {defaultTodos.map(todo => (
+      {searchedTodos.map(todo => (
         <TodoItem 
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
         />
       ))}
       </TodoList>
