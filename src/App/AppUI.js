7248d632ced3ed0b37e15ea3b0e2f33c8211dda0
Urutau <xristinaortega@gmail.com>
2024-06-24 17:04:07 -0300

    Vamos por el LocalStorage

---
+++ src/App/AppUI.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+
+function AppUI ({
+    loading,
+    error,
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo
+  }) {
+    
+    return (
+        <>
+        <TodoCounter 
+        completed = {completedTodos}
+        total = {totalTodos}
+        />
+  
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+  
+        <TodoList>
+        {loading && <p>Estamos cargando...</p>}
+        {error && <p>Estamos arreglando el problema...</p>}
+        {(!loading && searchedTodos.length == 0) && <p>¡Crea tu primer ToDo!</p>}
+
+
+        {searchedTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+        </TodoList>
+  
+        <CreateTodoButton />
+      </>
+    );
+}
+
+export { AppUI };
